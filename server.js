1a7c58c824ce7e586ab8b2feb3cb0f7d8c48e9cc
AikoFTW <130309866+AikoFTW@users.noreply.github.com>
2026-04-01 13:00:29 -0700

    update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 3000;
 
 // Security middleware
 app.use(helmet({
